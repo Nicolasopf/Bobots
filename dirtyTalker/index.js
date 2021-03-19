9089b406c6f10ca2752c1e55590cc94319fb0b77
root <n.ico15935746@gmail.com>
2021-03-19 00:39:45 +0000

    [Updated] Detect channel ID and the bot works only there

--- dirtyTalker/index.js
+++ dirtyTalker/index.js
@@ -2,14 +2,24 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
 const prefix = "!";
-client.login(config.BOT_TOKEN);
-
-setInterval(() => {
-    var channnel = client.channels.cache.get('ID'); // YOU SHOULD PUT THE CHANNEL ID HERE
-    channnel.send("MESSAGE HERE"); // PUT YOUR MESSAGE HERE
-}, 5000);
+var channnel = {};
 
 client.on("message", function(message) {
     if (!message.content.startsWith(prefix)) return;
-    message.reply("Hello cock sucker");
+    if (message.content === "!start") { // If user type !start in a channel, the bot will start giving automatic message every 1 minute
+	message.reply(typeof(message.channel.id));
+	channnel.id = client.channels.cache.get(message.channel.id);
+	auto_msg();
+    }
+    else message.reply('Hello cock sucker');
 });
+
+function auto_msg() {
+    setInterval(() => {
+	channnel.id.send('MESSAGE HERE'); // PUT YOUR MESSAGE HERE
+    }, 1000);
+};
+
+
+// Start the bot
+client.login(config.BOT_TOKEN);
