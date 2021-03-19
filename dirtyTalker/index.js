3008a6c1f62f33869711b15b87d4772187312495
Xvaria <2349@holbertonschool.com>
2021-03-19 19:51:39 +0000

    Correction v2.01 tag all users in the guild

--- dirtyTalker/index.js
+++ dirtyTalker/index.js
@@ -3,12 +3,26 @@ const config = require("./config.json");
 const client = new Discord.Client();
 const prefix = "!";
 var channnel = {};
+var list = {};
+//var memberlist = {};
+//var randmem = {};
+//var memmber = {};
 
 client.on("message", function(message) {
     if (!message.content.startsWith(prefix)) return;
-    if (message.content === "!start") { // If user type !start in a channel, the bot will start giving automatic message every 1 minute
+    if (message.content === "!start") { // If user type !start in a channel, the bot will start giving automatic message every 1 minut
 	message.reply(typeof(message.channel.id));
 	channnel.id = client.channels.cache.get(message.channel.id);
+	list.id = client.guilds.cache.get(message.guild.id);
+	message.reply(list.id.memberCount);
+	list.id.members.cache.forEach(member => {
+	    message.reply("<@"+member.id+">")
+	    //memmber.id.push(member.id);
+	});
+	//message.reply(typeof(member.id.user.username));
+	//randmem.id = memberlist.id[Math.floor(Math.random() * memberlist.id.length)]
+	//message.reply(typeof(randmem.id));
+	//console.log(memberlist.id);
 	auto_msg();
     }
     else message.reply('Hello cock sucker');
